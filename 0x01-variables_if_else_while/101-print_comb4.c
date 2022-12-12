3d2544b81f1c440fdcebc169847c3ed469b7aca7
khalx02 <ibaarolawa@gmail.com>
2022-12-12 13:20:58 -0800

    v101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,9 +2,9 @@
 /**
  *  * main - entry point
  *
- *  * main - Print all possible combinations of two-digits numbers,
+ *  * main - Print all possible combinations of three diffent digits,
  *
- *  * ranging from 0-99, separated by a comma followed by a space.
+ *  * in ascending order, separated by a comma followed by a space.
  *
  *  * Return: Always 0
  *
@@ -12,21 +12,22 @@
  */
 int main(void)
 {
-	int num1, num2;
+	int digit1, digit2, digit3;
 
-	for (num1 = 0; num1 <= 98; num1++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		for (num2 = num1 + 1; num2 <= 99; num2++)
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
 		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
-			if (num1 == 98 && num2 == 99)
-				continue;
-			putchar(',');
-			putchar(' ');
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			{
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
