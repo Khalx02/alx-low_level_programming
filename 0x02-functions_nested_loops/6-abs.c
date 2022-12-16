65318facd304c880f1cdb29c5a71cdc61b7244f3
khalx02 <ibaarolawa@gmail.com>
2022-12-16 06:11:38 -0800

    f6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,27 +1,17 @@
 #include "main.h"
 /**
- *times_table - Prints the 9 times table, starting with 0.
+ *_abs - Computes the absolute value of an integer.
+ *
+ *@n: The integer to be computed.
+ *
+ *Return: The absolute value of the integer.
  */
-
-void times_table(void)
+int _abs(int n)
 
 {
-	int num, mult, prod;
-
-	for (num = 0; num <= 9; num++)
-	{
-		_putchar('0');
-		for (mult = 1; mult <= 9; mult++)
-		{
-			_putchar(',');
-			_putchar(' ');
-			prod = num * mult;
-			if (prod <= 9)
-				_putchar(' ');
-			else
-				_putchar((prod / 10) + '0');
-		_putchar((prod % 10) + '0');
-		}
-		_putchar('\n');
-	}
+if (n >= 0)
+return (n);
+else
+return (-n);
+}
 }
