0dcaa7f1fc12dddbd04e89b260d89eec9ea93d8d
khalx02 <ibaarolawa@gmail.com>
2023-01-02 15:36:53 -0800

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,14 +1,14 @@
 #include "main.h"
- /**
- * _strspn - search a string for a set of bytes
- *
- * @s: source string
- *
- * @accept: accepted string
- *
- * Return: number of bytes in the init segment
- *
- */
+/**
+* _strspn - search a string for a set of bytes
+*
+* @s: source string
+*
+* @accept: accepted string
+*
+* Return: number of bytes in the init segment
+*
+*/
 
  unsigned int _strspn(char *s, char *accept)
 
